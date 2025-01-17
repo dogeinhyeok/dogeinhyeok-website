aed0f6fb8e94f5ba6d05187cc14dd88c22da73d7
devinhyeok <devinhyeok@gmail.com>
2025-01-17 20:00:20 +0900

    feat/navbar/logo

--- src/theme/Navbar/Logo/index.tsx
+++ src/theme/Navbar/Logo/index.tsx
@@ -1,12 +1,18 @@
-import React, {type ReactNode} from 'react';
-import Logo from '@theme/Logo';
+import React, { type ReactNode } from "react";
+import Logo from "@theme/Logo";
+import { useWindowSize } from "@docusaurus/theme-common"; // 윈도우 크기 확인
 
 export default function NavbarLogo(): ReactNode {
+  const windowSize = useWindowSize(); // 현재 창 크기
+  const isMobile = windowSize === "mobile"; // 모바일 모드인지 확인
+
   return (
     <Logo
       className="navbar__brand"
       imageClassName="navbar__logo"
-      titleClassName="navbar__title text--truncate"
+      titleClassName={`navbar__title text--truncate ${
+        isMobile ? "hidden" : ""
+      }`} // 모바일 모드에서 텍스트 숨기기
     />
   );
 }
